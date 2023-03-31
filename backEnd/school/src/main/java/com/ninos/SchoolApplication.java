1c1172627a0671f3b9ddaad257c7ed4a48e3f285
NinoosMoshi <nalkaton@gmail.com>
2023-03-31 17:06:44 -0500

    command line runner

--- backEnd/school/src/main/java/com/ninos/SchoolApplication.java
+++ backEnd/school/src/main/java/com/ninos/SchoolApplication.java
@@ -1,34 +1,15 @@
 package com.ninos;
 
-import com.ninos.dao.*;
-import com.ninos.utility.OperationUtility;
 import lombok.AllArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @AllArgsConstructor
 @SpringBootApplication
-public class SchoolApplication { //implements CommandLineRunner
-
-//	private final UserDao userDao;
-//	private final CourseDao courseDao;
-//	private final InstructorDao instructorDao;
-//	private final StudentDao studentDao;
-//	private final RoleDao roleDao;
+public class SchoolApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolApplication.class, args);
 	}
 
-//	@Override
-//	public void run(String... args) throws Exception {
-////		OperationUtility.userOperations(userDao);
-////		OperationUtility.roleOperations(roleDao);
-////		OperationUtility.assignRolesToUsers(userDao, roleDao);
-////		OperationUtility.instructorOperations(userDao,instructorDao,roleDao);
-////		OperationUtility.studentOperations(userDao,studentDao,roleDao);
-////		OperationUtility.coursesOperations(courseDao, instructorDao,studentDao);
-//
-//	}
 }
