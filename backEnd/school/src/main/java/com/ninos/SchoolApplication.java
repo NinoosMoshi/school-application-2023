34fe9bf691f8ce9cec5ff9850a2bdf1118b51811
NinoosMoshi <nalkaton@gmail.com>
2023-04-02 12:34:28 -0500

    add swagger

--- backEnd/school/src/main/java/com/ninos/SchoolApplication.java
+++ backEnd/school/src/main/java/com/ninos/SchoolApplication.java
@@ -1,10 +1,9 @@
 package com.ninos;
 
-import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@AllArgsConstructor
+
 @SpringBootApplication
 public class SchoolApplication {
 
